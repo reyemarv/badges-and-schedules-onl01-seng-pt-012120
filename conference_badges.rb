4d084669cf0ee9fdeeb883a51cccdb94c0fa5670
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 00:21:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+ attendees.map do |attendee|
+   "Hello, my name is #{attendee}."
+ end
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end 
+  
