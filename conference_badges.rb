516079ec4c6511aa4a677792359ec6fa02e2573c
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 00:28:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,4 +15,9 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each do |badge|
+  end
+  
+  assign_rooms(attendees).each do |speaker|
+  end
+end
